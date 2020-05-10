9ea3e62b785e083188cf9f29332db48fc98f411d
yjhcpdd <yjhc_pdd@126.com>
2020-05-10 22:21:16 +0800

    test

---
+++ scaf-tmp/src/test/java/TmpSpringTest.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TmpSpringTest {
+    
+}
