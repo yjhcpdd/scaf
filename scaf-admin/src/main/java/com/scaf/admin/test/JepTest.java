d8a9d5f5a6dc29d134de9c2e2e8c5a9e88e37b03
yjhcpdd <yjhc_pdd@126.com>
2020-06-03 21:13:00 +0800

    jep

---
+++ scaf-admin/src/main/java/com/scaf/admin/test/JepTest.java
@@ -0,0 +1,20 @@
+package com.scaf.admin.test;
+
+import org.nfunk.jep.JEP;
+
+public class JepTest {
+    
+    public static void main(String[] args) {
+        String expression="(a+b)*b-1";
+        JEP jep=new JEP();
+        //(1)设置变量
+        jep.addVariable("a",1);
+        jep.addVariable("b",2);
+        //(2)转换表达式
+        jep.parseExpression(expression);
+        //(3)获取表达式的值
+        double result=jep.getValue();
+        System.out.println(result);
+    }
+    
+}
